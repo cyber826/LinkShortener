ebecb6eeaa3412ebdd20b45b2521da590e7bff96
cyber826 <165576400+cyber826@users.noreply.github.com>
2023-12-23 22:44:17 +0800

    update installation instructions

--- admin/src/main/java/com/yiyuandev/abitoflink/admin/common/biz/user/UserContext.java
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/common/biz/user/UserContext.java
@@ -1,1 +1,22 @@
-package com.y
+package com.yiyuandev.abitoflink.admin.common.biz.user;
+
+import com.alibaba.ttl.TransmittableThreadLocal;
+
+import java.util.Optional;
+
+public final class UserContext {
+
+    /**
+     * <a href="https://github.com/alibaba/transmittable-thread-local">TTL: TransmittableThreadLocal</a>
+     */
+    private static final ThreadLocal<UserInfoDTO> USER_THREAD_LOCAL = new TransmittableThreadLocal<>();
+
+    /**
+     * Set user context
+     */
+    public static void setUser(UserInfoDTO user) {
+        USER_THREAD_LOCAL.set(user);
+    }
+
+    /**
+     * get user id 
