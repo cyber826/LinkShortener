697c3b78c2d64c9531ae4fde3433eab4438f1a9c
cyber826 <165576400+cyber826@users.noreply.github.com>
2023-12-26 18:02:35 +0800

    Update bitcoingui.cpp

--- admin/src/main/java/com/yiyuandev/abitoflink/admin/common/convention/exception/AbstractException.java
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/common/convention/exception/AbstractException.java
@@ -2,4 +2,26 @@ package com.yiyuandev.abitoflink.admin.common.convention.exception;
 
 import com.yiyuandev.abitoflink.admin.common.convention.errorcode.IErrorCode;
 import lombok.Getter;
-import org.springframework.util
+import org.springframework.util.StringUtils;
+
+import java.util.Optional;
+
+/**
+ * This abstracts the three types of exception hierarchies in the project:
+ * client exceptions, service exceptions, and remote exceptions
+ *
+ * @see ClientException
+ * @see ServiceException
+ * @see RemoteException
+ */
+@Getter
+public abstract class AbstractException extends RuntimeException {
+
+    public final String errorCode;
+
+    public final String errorMessage;
+
+    public AbstractException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable);
+        this.errorCode = errorCode.code();
+        this.errorMessag
