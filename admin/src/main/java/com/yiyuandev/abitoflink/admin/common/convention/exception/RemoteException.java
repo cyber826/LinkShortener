02b46589e2e1d8ea7e5a758efeccd4b22ad6fc11
cyber826 <165576400+cyber826@users.noreply.github.com>
2023-12-26 22:50:51 +0800

    f2fs: fix wrong memory condition check

--- admin/src/main/java/com/yiyuandev/abitoflink/admin/common/convention/exception/RemoteException.java
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/common/convention/exception/RemoteException.java
@@ -1,29 +1,26 @@
+
 package com.yiyuandev.abitoflink.admin.common.convention.exception;
 
 import com.yiyuandev.abitoflink.admin.common.convention.errorcode.BaseErrorEnum;
 import com.yiyuandev.abitoflink.admin.common.convention.errorcode.IErrorCode;
 
-public class ClientException extends AbstractException {
+public class RemoteException extends AbstractException {
 
-    public ClientException(IErrorCode errorCode) {
-        this(null, null, errorCode);
+    public RemoteException(String message) {
+        this(message, null, BaseErrorEnum.REMOTE_ERROR);
     }
 
-    public ClientException(String message) {
-        this(message, null, BaseErrorEnum.CLIENT_ERROR);
-    }
-
-    public ClientException(String message, IErrorCode errorCode) {
+    public RemoteException(String message, IErrorCode errorCode) {
         this(message, null, errorCode);
     }
 
-    public ClientException(String message, Throwable throwable, IErrorCode errorCode) {
+    public RemoteException(String message, Throwable throwable, IErrorCode errorCode) {
         super(message, throwable, errorCode);
     }
 
     @Override
     public String toString() {
-        return "ClientException{" +
+        return "RemoteException{" +
                 "code='" + errorCode + "'," +
                 "message='" + errorMessage + "'" +
                 '}';
---
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/common/convention/exception/ServiceException.java
@@ -0,0 +1,1 @@
+package com.yiyuandev.abitoflink.admin.common.convention.exc
