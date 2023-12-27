64f5db2279da6b30611dc1893f8cbe4983a6bfab
cyber826 <165576400+cyber826@users.noreply.github.com>
2023-12-27 17:29:07 +0800

    Add PKGBUILD

--- admin/src/main/java/com/yiyuandev/abitoflink/admin/common/convention/result/Result.java
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/common/convention/result/Result.java
@@ -4,4 +4,20 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 
 import java.io.Serial;
-import java.io.Serializabl
+import java.io.Serializable;
+
+@Data
+@Accessors(chain = true)
+public class Result<T> implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 5679018624309023727L;
+
+    public static final String SUCCESS_CODE = "0";
+    private String code;
+    private String message;
+    private T data;
+    private String requestId;
+    public boolean isSuccess(){
+        return SUCCESS_CODE.equals(code);
+    }
+}
---
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/common/convention/result/Results.java
@@ -0,0 +1,5 @@
+package com.yiyuandev.abitoflink.admin.common.convention.result;
+
+
+import com.yiyuandev.abitoflink.admin.common.convention.errorcode.BaseErrorEnum;
+import com.yiyuandev.a
