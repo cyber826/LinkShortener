547cf399a9feb413f65876357fbede918ecb9ac6
cyber826 <165576400+cyber826@users.noreply.github.com>
2023-12-28 13:43:15 +0800

    Automated merge

--- admin/src/main/java/com/yiyuandev/abitoflink/admin/common/convention/result/Results.java
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/common/convention/result/Results.java
@@ -2,4 +2,30 @@ package com.yiyuandev.abitoflink.admin.common.convention.result;
 
 
 import com.yiyuandev.abitoflink.admin.common.convention.errorcode.BaseErrorEnum;
-import com.yiyuandev.a
+import com.yiyuandev.abitoflink.admin.common.convention.exception.AbstractException;
+
+import java.util.Optional;
+
+/**
+ * Result response constructor
+ */
+public final class Results {
+
+    /**
+     * Construct success response
+     */
+    public static Result<Void> success() {
+        return new Result<Void>()
+                .setCode(Result.SUCCESS_CODE);
+    }
+
+    /**
+     * Construct success response with data
+     */
+    public static <T> Result<T> success(T data) {
+        return new Result<T>()
+                .setCode(Result.SUCCESS_CODE)
+                .setData(data);
+    }
+
+   
