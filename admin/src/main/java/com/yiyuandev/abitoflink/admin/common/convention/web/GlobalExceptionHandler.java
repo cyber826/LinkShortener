a1fbc47fc3b9271e5d2328461d52ad319b4ac839
cyber826 <165576400+cyber826@users.noreply.github.com>
2023-12-30 05:30:33 +0800

    Fix call

--- admin/src/main/java/com/yiyuandev/abitoflink/admin/common/convention/web/GlobalExceptionHandler.java
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/common/convention/web/GlobalExceptionHandler.java
@@ -1,3 +1,14 @@
 package com.yiyuandev.abitoflink.admin.common.convention.web;
 
-im
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.StrUtil;
+import com.yiyuandev.abitoflink.admin.common.convention.errorcode.BaseErrorEnum;
+import com.yiyuandev.abitoflink.admin.common.convention.exception.AbstractException;
+import com.yiyuandev.abitoflink.admin.common.convention.result.Result;
+import com.yiyuandev.abitoflink.admin.common.convention.result.Results;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org
