f740edb46cb31d06c8ca07553ff815214f9164cd
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-01-03 17:41:00 +0800

    mac80211: fix non RCU-safe sta_list manipulation

--- admin/src/main/java/com/yiyuandev/abitoflink/admin/config/UserConfiguration.java
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/config/UserConfiguration.java
@@ -1,3 +1,16 @@
 package com.yiyuandev.abitoflink.admin.config;
 
-import com.yiyuandev.abitoflink.
+import com.yiyuandev.abitoflink.admin.common.biz.user.UserTransmitFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserConfiguration {
+
+    @Bean
+    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter() {
+        FilterRegistrationBean<UserTransmitFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserTransmitFilter());
+        registration.addUrlPatterns("/*");
+        registrat
