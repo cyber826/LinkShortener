c823158fa8463f8d28f8052d7ae2c3a416ab49f3
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-01-04 05:25:05 +0800

    wu

--- admin/src/main/java/com/yiyuandev/abitoflink/admin/controller/GroupController.java
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/controller/GroupController.java
@@ -6,4 +6,24 @@ import com.yiyuandev.abitoflink.admin.dto.req.LinkGroupSaveReqDTO;
 import com.yiyuandev.abitoflink.admin.dto.req.LinkGroupSortReqDTO;
 import com.yiyuandev.abitoflink.admin.dto.req.LinkGroupUpdateReqDTO;
 import com.yiyuandev.abitoflink.admin.dto.resp.LinkGroupRespDTO;
-import com.yiyuandev.abitofl
+import com.yiyuandev.abitoflink.admin.service.GroupService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class GroupController {
+
+    private final GroupService groupService;
+
+    /**
+     * Add new group
+     */
+    @PostMapping("/api/abitoflink/admin/v1/group")
+    public Result<Void> saveGroup(@RequestBody LinkGroupSaveReqDTO requestParam) {
+        groupService.saveGroup(requestParam.getName());
+        return Results.success();
+    }
+
