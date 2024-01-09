9b4a4a69e5c986b076efc8380441a9c5c187b692
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-01-09 21:07:46 +0800

    Doc updates.

--- admin/src/main/java/com/yiyuandev/abitoflink/admin/dto/resp/UserActualRespDTO.java
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/dto/resp/UserActualRespDTO.java
@@ -3,4 +3,13 @@ package com.yiyuandev.abitoflink.admin.dto.resp;
 import lombok.Data;
 
 /**
- * User actu
+ * User actual response object
+ */
+@Data
+public class UserActualRespDTO {
+    private Long id;
+    private String username;
+    private String realName;
+    private String phone;
+    private String email;
+}
---
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/dto/resp/UserRespDTO.java
@@ -0,0 +1,5 @@
+package com.yiyuandev.abitoflink.admin.dto.resp;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.yiyuandev.abitoflink.admin.common.serialize.PhoneDesensitizationSerializer;
+import lombok.
