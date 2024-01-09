dd306cc822c9084b24065b82e8945973d2fee8c1
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-01-09 22:27:48 +0800

    better printing

--- admin/src/main/java/com/yiyuandev/abitoflink/admin/dto/resp/UserRespDTO.java
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/dto/resp/UserRespDTO.java
@@ -2,4 +2,18 @@ package com.yiyuandev.abitoflink.admin.dto.resp;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.yiyuandev.abitoflink.admin.common.serialize.PhoneDesensitizationSerializer;
-import lombok.
+import lombok.Data;
+
+/**
+ * User response object
+ */
+@Data
+public class UserRespDTO {
+    private Long id;
+    private String username;
+    private String realName;
+
+    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
+    private String phone;
+    private String email;
+}
---
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/remote/dto/req/ShortLinkBatchCreateReqDTO.java
@@ -0,0 +1,4 @@
+package com.yiyuandev.abitoflink.admin.remote.dto.req;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data
