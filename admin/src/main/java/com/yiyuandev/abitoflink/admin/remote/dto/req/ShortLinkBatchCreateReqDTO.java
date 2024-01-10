d52a13a36ed7a3e6a39b612b8e076d2ef04872fa
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-01-10 16:14:13 +0800

    styleci

--- admin/src/main/java/com/yiyuandev/abitoflink/admin/remote/dto/req/ShortLinkBatchCreateReqDTO.java
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/remote/dto/req/ShortLinkBatchCreateReqDTO.java
@@ -1,4 +1,40 @@
 package com.yiyuandev.abitoflink.admin.remote.dto.req;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Data
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class ShortLinkBatchCreateReqDTO {
+
+    /**
+     * origin urls
+     */
+    private List<String> originUrls;
+
+    /**
+     * batch descriptions
+     */
+    private List<String> descriptions;
+
+    /**
+     * group id
+     */
+    private String gid;
+
+    /**
+     * created type 0：api 1：console
+     */
+    private Integer createdType;
+
+    /**
+     * valid date type 0：permanent 1：custom
+     */
+    private Integer validDateType;
+
+    /**
+     * valid date
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH
