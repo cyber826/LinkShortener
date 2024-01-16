d4147b0082c3ea4778e632c3c68db0f175a488e9
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-01-16 16:58:05 +0800

    Add travis

--- admin/src/main/java/com/yiyuandev/abitoflink/admin/service/RecycleBinService.java
+++ admin/src/main/java/com/yiyuandev/abitoflink/admin/service/RecycleBinService.java
@@ -1,1 +1,15 @@
-package com.yiyuandev.abitoflink.adm
+package com.yiyuandev.abitoflink.admin.service;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yiyuandev.abitoflink.admin.common.convention.result.Result;
+import com.yiyuandev.abitoflink.admin.remote.dto.req.ShortLinkRecycleBinPageReqDTO;
+import com.yiyuandev.abitoflink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+
+public interface RecycleBinService {
+    /**
+     * recycle bin short link pagination
+     *
+     * @param requestParam ShortLinkRecycleBinPageReqDTO
+     * @return IPage<ShortLinkPageRespDTO>
+     */
+    Result<Page<ShortLinkPageRespDTO>> pageRecycleBinShortLink(Sh
