b0aa2617c310bd64257fa1de8c446b13c929b759
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-01-20 22:04:05 +0800

    Add missing space.

--- admin/src/test/java/com/yiyuandev/abitoflink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/com/yiyuandev/abitoflink/admin/test/UserTableShardingTest.java
@@ -8,4 +8,14 @@ public class UserTableShardingTest {
             "  `real_name` varchar(256) DEFAULT NULL,\n" +
             "  `phone` varchar(128) DEFAULT NULL,\n" +
             "  `email` varchar(512) DEFAULT NULL,\n" +
-            " 
+            "  `deletion_time` bigint DEFAULT NULL,\n" +
+            "  `creation_time` datetime DEFAULT NULL,\n" +
+            "  `update_time` datetime DEFAULT NULL,\n" +
+            "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '0: not deleted; 1: deleted',\n" +
+            "  PRIMARY KEY (`id`),\n" +
+            "  UNIQUE KEY `idx_unique_username` (`username`) USING BTREE\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=1756999192868450306 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 16; i++) {
+           
