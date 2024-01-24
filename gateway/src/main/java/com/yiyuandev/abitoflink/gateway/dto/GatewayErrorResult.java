469e5663f6d421d786a413a71022e343919f424e
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-01-24 17:39:49 +0800

    modify name

--- gateway/src/main/java/com/yiyuandev/abitoflink/gateway/dto/GatewayErrorResult.java
+++ gateway/src/main/java/com/yiyuandev/abitoflink/gateway/dto/GatewayErrorResult.java
@@ -2,4 +2,22 @@ package com.yiyuandev.abitoflink.gateway.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-imp
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GatewayErrorResult {
+
+    /**
+     * HTTP status
+     */
+    private Integer status;
+
+    /**
+     * return message
+     */
+    private String message;
+}
---
+++ gateway/src/main/java/com/yiyuandev/abitoflink/gateway/filter/TokenValidateGatewayFilterFactory.java
@@ -0,0 +1,8 @@
+package com.yiyuandev.abitoflink.gateway.filter;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.yiyuandev.abitoflink.gateway.config.Config;
+import com.yiyuandev.abitoflink.gateway.dto.GatewayErrorResult;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springf
