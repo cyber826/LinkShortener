d75fd3d375571c2777fb6a886880383cfe58c185
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-02-03 21:27:43 +0800

    Code style fix.

--- project/src/main/java/com/yiyuandev/abitoflink/project/common/convention/exception/AbstractException.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/common/convention/exception/AbstractException.java
@@ -7,4 +7,21 @@ import org.springframework.util.StringUtils;
 import java.util.Optional;
 
 /**
- * This abstracts the three types of
+ * This abstracts the three types of exception hierarchies in the project:
+ * client exceptions, service exceptions, and remote exceptions
+ *
+ * @see ClientException
+ * @see ServiceException
+ * @see RemoteException
+ */
+@Getter
+public abstract class AbstractException extends RuntimeException {
+
+    public final String errorCode;
+
+    public final String errorMessage;
+
+    public AbstractException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable);
+        this.errorCode = errorCode.code();
+        this.errorMessage = Optional.ofNullable(StringU
