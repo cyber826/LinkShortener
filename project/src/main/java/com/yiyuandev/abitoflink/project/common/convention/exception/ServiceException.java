6c0ccc94755ead1214771e6749aad7bf44efa3c8
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-02-05 05:42:51 +0800

    ALSA: rawmidi: Remove kernel WARNING for NULL user-space buffer check

--- project/src/main/java/com/yiyuandev/abitoflink/project/common/convention/exception/ServiceException.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/common/convention/exception/ServiceException.java
@@ -1,3 +1,22 @@
 package com.yiyuandev.abitoflink.project.common.convention.exception;
 
-import com.yiyuandev.abitoflink.project.common.convention.errorco
+import com.yiyuandev.abitoflink.project.common.convention.errorcode.BaseErrorEnum;
+import com.yiyuandev.abitoflink.project.common.convention.errorcode.IErrorCode;
+
+import java.util.Optional;
+
+public class ServiceException extends AbstractException {
+
+    public ServiceException(String message) {
+        this(message, null, BaseErrorEnum.SERVICE_ERROR);
+    }
+
+    public ServiceException(IErrorCode errorCode) {
+        this(null, errorCode);
+    }
+
+    public ServiceException(String message, IErrorCode errorCode) {
+        this(message, null, errorCode);
+    }
+
+    public ServiceException(String message, Throwable throwable, IEr
