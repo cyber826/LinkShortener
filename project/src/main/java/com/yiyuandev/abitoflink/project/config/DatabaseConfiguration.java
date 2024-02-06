e72d8e2f1a197f18cb3072ba2a564b85eb62937a
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-02-06 18:00:16 +0800

    poprawki

--- project/src/main/java/com/yiyuandev/abitoflink/project/config/DatabaseConfiguration.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/config/DatabaseConfiguration.java
@@ -1,2 +1,21 @@
 
 package com.yiyuandev.abitoflink.project.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
---
+++ project/src/main/java/com/yiyuandev/abitoflink/project/config/MyMetaObjectHandler.java
@@ -0,0 +1,14 @@
+package com.yiyuandev.abitoflink.project.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "creationTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "updateT
