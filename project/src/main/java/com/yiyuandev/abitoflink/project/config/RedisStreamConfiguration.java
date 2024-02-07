ec875391645a93e50971f51afe4291aab3a1d7b3
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-02-07 20:13:43 +0800

    Update invite.lua

--- project/src/main/java/com/yiyuandev/abitoflink/project/config/RedisStreamConfiguration.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/config/RedisStreamConfiguration.java
@@ -2,4 +2,14 @@ package com.yiyuandev.abitoflink.project.config;
 
 import com.yiyuandev.abitoflink.project.mq.consumer.ShortLinkStatsSaveConsumer;
 import lombok.RequiredArgsConstructor;
-import org.springframework.
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.stream.Consumer;
+import org.springframework.data.redis.connection.stream.MapRecord;
+import org.springframework.data.redis.connection.stream.ReadOffset;
+import org.springframework.data.redis.connection.stream.StreamOffset;
+import org.springframework.data.redis.stream.StreamMessageListenerContainer;
+
+import java.time.Duration;
+import java.util.concurrent.ExecutorServi
