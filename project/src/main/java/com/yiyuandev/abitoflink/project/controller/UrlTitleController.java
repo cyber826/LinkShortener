d123cd6d3b837f7ea48ef6a3acf437b8889c9ae2
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-02-13 05:33:26 +0800

    Little correction

--- project/src/main/java/com/yiyuandev/abitoflink/project/controller/UrlTitleController.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/controller/UrlTitleController.java
@@ -1,3 +1,18 @@
 package com.yiyuandev.abitoflink.project.controller;
 
-import com.yiyuandev.abitoflink.project.com
+import com.yiyuandev.abitoflink.project.common.convention.result.Result;
+import com.yiyuandev.abitoflink.project.common.convention.result.Results;
+import com.yiyuandev.abitoflink.project.service.UrlTitleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UrlTitleController {
+
+    private final UrlTitleService urlTitleService;
+
+    /**
+     * get title of the webpage from ur
