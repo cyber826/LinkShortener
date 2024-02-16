5175021430c3237f1396452b06b8e2a9d4725a91
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-02-16 12:50:58 +0800

    Builder4/Editor.bjs Builder4/Editor.js Builder4/Editor.vala

--- project/src/main/java/com/yiyuandev/abitoflink/project/dao/entity/LinkLocaleStatsDO.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/dao/entity/LinkLocaleStatsDO.java
@@ -1,3 +1,28 @@
 package com.yiyuandev.abitoflink.project.dao.entity;
 
-impo
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.yiyuandev.abitoflink.project.common.database.BaseDO;
+import lombok.*;
+
+import java.util.Date;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("t_link_locale_stats")
+public class LinkLocaleStatsDO extends BaseDO {
+
+    /**
+     * id
+     */
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long id;
+
+    /**
+     * group id
+     */
+    private St
