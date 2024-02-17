b4fcc9b15f4084c173ee0f79eb3ddad20e3d344f
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-02-17 08:37:39 +0800

    Update expectations

--- project/src/main/java/com/yiyuandev/abitoflink/project/dao/entity/LinkNetworkStatsDO.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/dao/entity/LinkNetworkStatsDO.java
@@ -1,3 +1,31 @@
 package com.yiyuandev.abitoflink.project.dao.entity;
 
-import com.baomidou.mybatisplus.annota
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.yiyuandev.abitoflink.project.common.database.BaseDO;
+import lombok.*;
+
+import java.util.Date;
+
+/**
+ * Browser stats
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("t_link_network_stats")
+public class LinkNetworkStatsDO extends BaseDO {
+
+    /**
+     * id
+     */
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long id;
+
+    /**
+     * group id
+     */
+    private String gid;
