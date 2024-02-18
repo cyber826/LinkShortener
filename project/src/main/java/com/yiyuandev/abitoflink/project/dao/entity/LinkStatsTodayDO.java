ed0605c405a2358b9807e37c67239d60592d7020
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-02-18 19:41:29 +0800

    ip6_gre: release cached dst on tunnel removal

--- project/src/main/java/com/yiyuandev/abitoflink/project/dao/entity/LinkStatsTodayDO.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/dao/entity/LinkStatsTodayDO.java
@@ -1,1 +1,22 @@
-pa
+package com.yiyuandev.abitoflink.project.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.yiyuandev.abitoflink.project.common.database.BaseDO;
+import lombok.*;
+
+import java.util.Date;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("t_link_stats_today")
+public class LinkStatsTodayDO extends BaseDO {
+
+    /**
+     * id
+     */
+    @TableId(type = Id
