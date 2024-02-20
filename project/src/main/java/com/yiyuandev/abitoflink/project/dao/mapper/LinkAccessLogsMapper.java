d10caca7a5a5532a7f3ab32bc29635f7100e5fc5
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-02-21 07:27:11 +0800

    Update ARCHITECTURE.md

--- project/src/main/java/com/yiyuandev/abitoflink/project/dao/mapper/LinkAccessLogsMapper.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/dao/mapper/LinkAccessLogsMapper.java
@@ -8,4 +8,20 @@ import com.yiyuandev.abitoflink.project.dto.req.ShortLinkStatsReqDTO;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
-import ja
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public interface LinkAccessLogsMapper extends BaseMapper<LinkAccessLogsDO> {
+    /**
+     * Retrieve high-frequency ip access data within a specified date range
+     */
+    @Select("SELECT " +
+            "    ip, " +
+            "    COUNT(ip) AS count " +
+            "FROM " +
+            "    t_link_access_logs " +
+            "WHERE " +
+            "    full_short_url = #{param.fullShortUrl} " +
+            "    AND gid = #{param.gid} " +
+            "    AND c
