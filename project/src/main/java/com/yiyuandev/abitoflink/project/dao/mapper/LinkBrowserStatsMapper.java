e1c4bf0c5f2f5d70355cab7847fb9f3b5b41673c
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-02-26 14:06:21 +0800

    watchdog: ts72xx_wdt: locking bug in ioctl

--- project/src/main/java/com/yiyuandev/abitoflink/project/dao/mapper/LinkBrowserStatsMapper.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/dao/mapper/LinkBrowserStatsMapper.java
@@ -16,4 +16,19 @@ public interface LinkBrowserStatsMapper extends BaseMapper<LinkBrowserStatsDO> {
     @Insert("INSERT INTO t_link_browser_stats (full_short_url, gid, date, cnt, browser, creation_time, update_time, del_flag )" +
             "VALUES" +
             "(#{linkBrowserStats.fullShortUrl}, #{linkBrowserStats.gid}, #{linkBrowserStats.date}, #{linkBrowserStats.cnt}, #{linkBrowserStats.browser}, NOW(), NOW(), 0 )" +
-            "ON DUPLICATE KEY UPDATE cnt = cnt + #{linkBrowserStats.c
+            "ON DUPLICATE KEY UPDATE cnt = cnt + #{linkBrowserStats.cnt};")
+    void shortLinkBrowserStats(@Param("linkBrowserStats") LinkBrowserStatsDO browserStatsDO);
+
+
+    @Select("SELECT " +
+            "    browser, " +
+            "    SUM(cnt) AS count " +
+            "FROM " +
+            "    t_link_browser_stats " +
+            "WHERE " +
+            "    full_short_url = #{param.fullShortUrl} " +
+            "    AND gid = #{param.gid} " +
+            "    AND date BETWEEN #{param.startDate} and ADDDATE(#{param.endDate}, 1) " +
+            "GROUP BY " +
+            "    full_short_url, gid, browser;")
+    List<HashMap<String
