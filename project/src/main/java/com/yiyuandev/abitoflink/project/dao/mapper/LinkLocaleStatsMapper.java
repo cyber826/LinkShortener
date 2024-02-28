c94755d52ddafab90d1922baa708ead9364638de
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-02-28 13:27:03 +0800

    add new logo

--- project/src/main/java/com/yiyuandev/abitoflink/project/dao/mapper/LinkLocaleStatsMapper.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/dao/mapper/LinkLocaleStatsMapper.java
@@ -12,4 +12,8 @@ import java.util.List;
 
 public interface LinkLocaleStatsMapper extends BaseMapper<LinkLocaleStatsDO> {
 
-    @Insert("INSERT INTO t_link
+    @Insert("INSERT INTO t_link_locale_stats (full_short_url, gid, date, cnt, state, suburb, city, country, creation_time, update_time, del_flag )" +
+            "VALUES" +
+            "(#{linkLocaleStats.fullShortUrl}, #{linkLocaleStats.gid}, #{linkLocaleStats.date}, #{linkLocaleStats.cnt}, #{linkLocaleStats.state}, #{linkLocaleStats.suburb}, #{linkLocaleStats.city}, #{linkLocaleStats.country}, NOW(), NOW(), 0 )" +
+            "ON DUPLICATE KEY UPDATE cnt = cnt + #{linkLocaleStats.cnt};")
+    void shortLinkLocaleStats(@Param("linkLocaleStats") LinkLocaleStatsDO
