ad5d32cee74930d3a44167f0ac49e2057a0383db
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-02-28 11:43:06 +0800

    Coq 8.11.0, repo released, mode clean.

--- project/src/main/java/com/yiyuandev/abitoflink/project/dao/mapper/LinkLocaleStatsMapper.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/dao/mapper/LinkLocaleStatsMapper.java
@@ -1,1 +1,15 @@
-package com.yiyuandev.abitoflink.project.dao.m
+package com.yiyuandev.abitoflink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yiyuandev.abitoflink.project.dao.entity.LinkLocaleStatsDO;
+import com.yiyuandev.abitoflink.project.dto.req.ShortLinkGroupStatsReqDTO;
+import com.yiyuandev.abitoflink.project.dto.req.ShortLinkStatsReqDTO;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface LinkLocaleStatsMapper extends BaseMapper<LinkLocaleStatsDO> {
+
+    @Insert("INSERT INTO t_link
