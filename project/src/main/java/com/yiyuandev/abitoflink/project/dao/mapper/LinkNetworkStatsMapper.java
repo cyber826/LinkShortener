fa7f5ef1615d24a32479bbb97643034c122b082c
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-02-29 11:56:29 +0800

    Remove .ruby-version

--- project/src/main/java/com/yiyuandev/abitoflink/project/dao/mapper/LinkNetworkStatsMapper.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/dao/mapper/LinkNetworkStatsMapper.java
@@ -1,4 +1,17 @@
 package com.yiyuandev.abitoflink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.yiyuandev.abit
+import com.yiyuandev.abitoflink.project.dao.entity.LinkNetworkStatsDO;
+import com.yiyuandev.abitoflink.project.dto.req.ShortLinkGroupStatsReqDTO;
+import com.yiyuandev.abitoflink.project.dto.req.ShortLinkStatsReqDTO;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface LinkNetworkStatsMapper extends BaseMapper<LinkNetworkStatsDO> {
+
+    @Insert("INSERT INTO t_link_network_stats (full_short_url, gid, date, cnt, network, creation_time, update_time, del_flag )" +
+            "VALUES" +
+            "(
