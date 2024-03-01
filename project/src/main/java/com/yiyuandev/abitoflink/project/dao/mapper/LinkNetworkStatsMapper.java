f169047e94761c617f8076281be4a110de091ff2
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-03-01 08:30:24 +0800

    modif gitignore

--- project/src/main/java/com/yiyuandev/abitoflink/project/dao/mapper/LinkNetworkStatsMapper.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/dao/mapper/LinkNetworkStatsMapper.java
@@ -14,4 +14,14 @@ public interface LinkNetworkStatsMapper extends BaseMapper<LinkNetworkStatsDO> {
 
     @Insert("INSERT INTO t_link_network_stats (full_short_url, gid, date, cnt, network, creation_time, update_time, del_flag )" +
             "VALUES" +
-            "(
+            "(#{linkNetworkStats.fullShortUrl}, #{linkNetworkStats.gid}, #{linkNetworkStats.date}, #{linkNetworkStats.cnt}, #{linkNetworkStats.network}, NOW(), NOW(), 0 )" +
+            "ON DUPLICATE KEY UPDATE cnt = cnt + #{linkNetworkStats.cnt};")
+    void shortLinkNetworkStats(@Param("linkNetworkStats") LinkNetworkStatsDO networkStatsDO);
+
+    @Select("SELECT " +
+            "    network, " +
+            "    SUM(cnt) AS cnt " +
+            "FROM " +
+            "    t_link_network_stats " +
+            "WHERE " +
+            "    full_short_url = #{p
