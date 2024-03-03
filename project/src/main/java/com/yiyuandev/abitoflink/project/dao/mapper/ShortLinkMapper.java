3c8e00a938ce5520f660796adc7e856f1457696e
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-03-03 16:27:56 +0800

    Translated using Weblate (Azerbaijani)

--- project/src/main/java/com/yiyuandev/abitoflink/project/dao/mapper/ShortLinkMapper.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/dao/mapper/ShortLinkMapper.java
@@ -1,4 +1,15 @@
 package com.yiyuandev.abitoflink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.co
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.yiyuandev.abitoflink.project.dao.entity.ShortLinkDO;
+import com.yiyuandev.abitoflink.project.dto.req.ShortLinkPageReqDTO;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
+
+public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
+
+    /**
+     * stats incremental
+     */
+    @Update("update t_link set total_pv = total_pv + #{totalPv}, total_uv = total_uv + #{totalUv}, total_uip = total_uip + #{totalUip} where gid = #{gid} and full_short_url = #{fullShortUrl}"
