1082c71a69dec573e89b66fb09df293a6500d06a
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-03-05 12:29:52 +0800

    [goal] Migrate testing to ubuntu focal

--- project/src/main/java/com/yiyuandev/abitoflink/project/dto/req/ShortLinkCreateReqDTO.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/dto/req/ShortLinkCreateReqDTO.java
@@ -9,4 +9,37 @@ import lombok.NoArgsConstructor;
 import java.util.Date;
 
 @Data
-@NoArgsConstruc
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ShortLinkCreateReqDTO {
+    /**
+     * domain
+     */
+    private String domain;
+
+    /**
+     * original url
+     */
+    private String originUrl;
+
+    /**
+     * group id
+     */
+    private String gid;
+
+    /**
+     * by api: 0， by console: 1
+     */
+    private int createdType;
+
+    /**
+     * permanent: 0， custom: 1
+     */
+    private int validDateType;
+
+    /**
+     * valid_date
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+11")
+    private Date validDate;
