2071cf906ec219c99096e12f3ab02d500422794e
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-03-06 14:49:05 +0800

    tiny tweak

--- project/src/main/java/com/yiyuandev/abitoflink/project/dto/resp/ShortLinkBaseInfoRespDTO.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/dto/resp/ShortLinkBaseInfoRespDTO.java
@@ -1,3 +1,31 @@
 package com.yiyuandev.abitoflink.project.dto.resp;
 
-import com.alibaba.excel.annotation.ExcelPropert
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ShortLinkBaseInfoRespDTO {
+
+    /**
+     * description column
+     */
+    @ExcelProperty("description")
+    @ColumnWidth(60)
+    private String description;
+
+    /**
+     * short link column
+     */
+    @ExcelProperty("short-link")
+    @ColumnWidth(60)
+    private String fullShortUrl;
+
+    /**
+     * origina
