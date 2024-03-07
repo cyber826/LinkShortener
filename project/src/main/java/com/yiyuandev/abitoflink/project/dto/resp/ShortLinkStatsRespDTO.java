a6054ac7bd179bb23606dc8c0c20cfd3258c0cd4
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-03-07 19:18:01 +0800

    Added javadoc comments.

--- project/src/main/java/com/yiyuandev/abitoflink/project/dto/resp/ShortLinkStatsRespDTO.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/dto/resp/ShortLinkStatsRespDTO.java
@@ -5,21 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ShortLinkStatsOsRespDTO {
+public class ShortLinkStatsRespDTO {
 
     /**
-     * count
-     */
-    private Integer cnt;
-
-    /**
-     * operating system
-     */
-    private String os;
-
-    private Double ratio;
-}
+   
