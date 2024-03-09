0df05d89b44cbd9ac2af8178a735025e7e43bfd5
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-03-09 21:20:11 +0800

    mv files

--- project/src/main/java/com/yiyuandev/abitoflink/project/mq/consumer/DelayShortLinkStatsConsumer.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/mq/consumer/DelayShortLinkStatsConsumer.java
@@ -3,4 +3,19 @@ package com.yiyuandev.abitoflink.project.mq.consumer;
 import com.yiyuandev.abitoflink.project.common.convention.exception.ServiceException;
 import com.yiyuandev.abitoflink.project.dto.biz.ShortLinkStatsRecordDTO;
 import com.yiyuandev.abitoflink.project.mq.idempotent.MessageQueueIdempotentHandler;
-import co
+import com.yiyuandev.abitoflink.project.service.ShortLinkService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RBlockingDeque;
+import org.redisson.api.RDelayedQueue;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.LockSupport;
+
+import static com.yiyuandev.abitoflink.project.common.constant.RedisKeyConstant.DELAY_QUEUE_STATS_KEY;
+
+@Slf4j
+@Component
