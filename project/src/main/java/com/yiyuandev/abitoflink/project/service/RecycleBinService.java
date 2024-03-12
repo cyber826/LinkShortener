ca0e77600c38976b760a0539fcc024894a54cc61
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-03-13 06:11:47 +0800

    simplification

--- project/src/main/java/com/yiyuandev/abitoflink/project/service/RecycleBinService.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/service/RecycleBinService.java
@@ -3,4 +3,15 @@ package com.yiyuandev.abitoflink.project.service;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.yiyuandev.abitoflink.project.dao.entity.ShortLinkDO;
-import com.yiyuandev.abitoflink.p
+import com.yiyuandev.abitoflink.project.dto.req.RecycleBinRecoverReqDTO;
+import com.yiyuandev.abitoflink.project.dto.req.RecycleBinRemoveReqDTO;
+import com.yiyuandev.abitoflink.project.dto.req.RecycleBinSaveReqDTO;
+import com.yiyuandev.abitoflink.project.dto.req.ShortLinkRecycleBinPageReqDTO;
+import com.yiyuandev.abitoflink.project.dto.resp.ShortLinkPageRespDTO;
+
+public interface RecycleBinService extends IService<ShortLinkDO> {
+    /**
+     * move to recycle bin
+     * @param requestParam RecycleBinSaveReqDTO
+     */
+    void saveRecycleBin(RecycleBinSaveReqDTO requestPar
