49871bbcb09589ed9bbe3bb584c3860eb63bc453
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-03-13 12:19:40 +0800

    remove some unused files

--- project/src/main/java/com/yiyuandev/abitoflink/project/service/ShortLinkStatsService.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/service/ShortLinkStatsService.java
@@ -1,1 +1,11 @@
-package com.yiyuandev.a
+package com.yiyuandev.abitoflink.project.service;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.yiyuandev.abitoflink.project.dto.req.ShortLinkGroupStatsAccessRecordReqDTO;
+import com.yiyuandev.abitoflink.project.dto.req.ShortLinkGroupStatsReqDTO;
+import com.yiyuandev.abitoflink.project.dto.req.ShortLinkStatsAccessRecordReqDTO;
+import com.yiyuandev.abitoflink.project.dto.req.ShortLinkStatsReqDTO;
+import com.yiyuandev.abitoflink.project.dto.resp.ShortLinkStatsAccessRecordRespDTO;
+import com.yiyuandev.abitoflink.project.dto.resp.ShortLinkStatsRespDTO;
+
+public interface ShortLinkStatsSer
