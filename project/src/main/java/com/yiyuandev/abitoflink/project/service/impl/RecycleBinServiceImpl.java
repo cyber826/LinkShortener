f004f7a8cfa883f2a460fea321f1aa12c4f2326e
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-03-15 08:12:09 +0800

    add python3 support

--- project/src/main/java/com/yiyuandev/abitoflink/project/service/impl/RecycleBinServiceImpl.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/service/impl/RecycleBinServiceImpl.java
@@ -16,4 +16,17 @@ import com.yiyuandev.abitoflink.project.dto.resp.ShortLinkPageRespDTO;
 import com.yiyuandev.abitoflink.project.service.RecycleBinService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.spri
+import org.springframework.stereotype.Service;
+
+import static com.yiyuandev.abitoflink.project.common.constant.RedisKeyConstant.GOTO_IS_NULL_SHORT_LINK_KEY;
+import static com.yiyuandev.abitoflink.project.common.constant.RedisKeyConstant.GOTO_SHORT_LINK_KEY;
+
+@Service
+@RequiredArgsConstructor
+public class RecycleBinServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLinkDO> implements RecycleBinService {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public void saveRecycleBin(RecycleBinSaveReqDTO requestParam) {
+        LambdaUpdateWrappe
