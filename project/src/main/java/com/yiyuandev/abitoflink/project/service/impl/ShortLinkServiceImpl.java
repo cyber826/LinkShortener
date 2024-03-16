f69596e4e2465e5b8dc9b3a83875c1bfdb0fde3e
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-03-17 07:02:51 +0800

    OEML SDK generator

--- project/src/main/java/com/yiyuandev/abitoflink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/service/impl/ShortLinkServiceImpl.java
@@ -2,4 +2,15 @@ package com.yiyuandev.abitoflink.project.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
-import cn
+import cn.hutool.core.lang.UUID;
+import cn.hutool.core.text.StrBuilder;
+import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson2.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import co
