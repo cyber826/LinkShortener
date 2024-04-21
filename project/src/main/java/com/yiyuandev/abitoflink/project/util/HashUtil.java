81eba25588747b642315deded1df549f45151aff
cyber826 <165576400+cyber826@users.noreply.github.com>
2024-04-22 07:07:00 +0800

    release 0.3.6

--- project/src/main/java/com/yiyuandev/abitoflink/project/util/HashUtil.java
+++ project/src/main/java/com/yiyuandev/abitoflink/project/util/HashUtil.java
@@ -4,4 +4,16 @@ import cn.hutool.core.lang.hash.MurmurHash;
 
 public class HashUtil {
 
-    private static final char[] CHAR
+    private static final char[] CHARS = new char[]{
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    };
+
+    private static final int SIZE = CHARS.length;
+
+    private static String convertDecToBase62(long num) {
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            int i = (int) (num % SIZE);
+            sb.append(CH
